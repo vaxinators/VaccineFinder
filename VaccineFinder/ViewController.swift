8ae4fba87a5e777b6f48db51410b56fd26b0ae80
Favian Flores <floresf1997@gmail.com>
2021-04-11 12:07:17 -0400

    first commit

---
+++ VaccineFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VaccineFinder
+//
+//  Created by Favian Flores on 4/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
