66a7eeef03e37e2bc9dd210816562d58959cdcdb
Sophie <sophiee.ongg@gmail.com>
2021-05-21 19:48:01 -0400

    adding shadows

--- VaccineFinder/ApptTableViewCell.swift
+++ VaccineFinder/ApptTableViewCell.swift
@@ -15,6 +15,13 @@ class ApptTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        // adding shadows
+        /*
+        locationName.layer.shadowColor = UIColor.white.cgColor
+        locationName.layer.shadowOffset = .zero
+        locationName.layer.shadowOpacity = 0.6
+        locationName.layer.shadowRadius = 10.0
+ */
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
