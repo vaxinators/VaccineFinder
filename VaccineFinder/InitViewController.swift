fb26d4077eb75a24cb5471abda923339816ac85e
jackson.lu02@myhunter.cuny.edu <jackson.lu02@myhunter.cuny.edu>
2021-05-28 11:31:57 -0400

    dismiss keyboard

--- VaccineFinder/InitViewController.swift
+++ VaccineFinder/InitViewController.swift
@@ -18,8 +18,31 @@ class InitViewController: UIViewController {
 		UserDefaults.standard.setValue("00000", forKey: "userLocation")
 		UserDefaults.standard.setValue(false, forKey: "showOnlyIfAvailable")
 		UserDefaults.standard.setValue(10, forKey: "maxDistance")
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
 	}
 
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if self.view.frame.origin.y == 0 {
+            self.view.frame.origin.y -= 100
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    @objc func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        zipCodeTF.resignFirstResponder()
+    }
+    
 	@IBAction func onGoButton(_ sender: Any) {
 		let addressString = zipCodeTF.text!
 		let geocoder = CLGeocoder()
